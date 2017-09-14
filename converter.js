821cfd6c17ef4e9907793b976a39e8839ac9be6d
Matias De Carli <matiasdecarli@gmail.com>
2017-09-14 12:34:51 -0300

    added to kelvin function

--- converter.js
+++ converter.js
@@ -1,9 +1,28 @@
 module.exports = (function () {
+  const r1 = 10000.0
+  const constantA = 1.129148e-3
+  const constantB = 2.34125e-4
+  const constantC = 8.76741e-8
+
   function toCelsius (value) {
     return value - 100
   }
 
+  function toKalvin (adc) {
+    let adcVolt = adc * 0.488
+    let adcRes = (adcVolt * r1) / (500 - adcVolt)
+    return stainHart(adcRes) - 2742
+  }
+
+  function stainHart (r) {
+    let logR = Math.log2(r)
+    let logR3 = logR * logR * logR
+
+    return 10 / (constantA + (constantB * logR) + (constantC * logR3))
+  }
+
   return {
-    toCelsius
+    toCelsius,
+    toKalvin
   }
 })()
--- test/index.test.js
+++ test/index.test.js
@@ -3,10 +3,31 @@
 const assert = require('assert')
 const converter = require('../converter.js')
 
-describe('should conver to acd', function () {
+describe('should conver to acd to celsius', function () {
   it('to celsius', function () {
     let val = 500
-    assert.equal(converter.toCelsius(val), val-100)
+    assert.equal(converter.toCelsius(val), val - 100)
+  })
+})
 
+describe('should conver to acd to celsius', function () {
+  it('100', function () {
+    assert.equal(converter.toKalvin(100), 52.360913671096114)
+  })
+
+  it('200', function () {
+    assert.equal(converter.toKalvin(200), -168.8532311940894)
+  })
+
+  it('300', function () {
+    assert.equal(converter.toKalvin(300), -299.6207544896333)
+  })
+
+  it('400', function () {
+    assert.equal(converter.toKalvin(400), -398.362268980728)
+  })
+
+  it('500', function () {
+    assert.equal(converter.toKalvin(500), -482.8338235622623)
   })
 })
