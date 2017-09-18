3d58db68c3f4effd438ee09ad332ae50896ed551
Matias De Carli <matiasdecarli@gmail.com>
2017-09-18 14:09:04 -0300

    final fixes

--- converter.js
+++ converter.js
@@ -1,24 +1,27 @@
 module.exports = (function () {
-  const r1 = 10000.0
   const constantA = 1.129148e-3
   const constantB = 2.34125e-4
   const constantC = 8.76741e-8
+  const V_IN = 5
+  const R1 = 10000
+  const K = 3.0
 
-  function toCelsius (value) {
-    return toKelvin(value) - 273.15
+  function SteinHart (R) {
+    let logR = Math.log(R)
+    let logR3 = logR * logR * logR
+    return 1.0 / (constantA + constantB * logR + constantC * logR3)
   }
 
   function toKelvin (adc) {
-    let adcVolt = adc * 0.488
-    let adcRes = (adcVolt * r1) / (500 - adcVolt)
-    return stainHart(adcRes) - 2742
+    let adcRaw = 1023 - adc
+    let adcRes = (adcRaw * R1) / (1023 - adcRaw)
+    let adcVolt = (adcRaw * V_IN) / 1023
+
+    return SteinHart(adcRes) - (adcVolt * adcVolt) / (K * adcRes)
   }
 
-  function stainHart (r) {
-    let logR = Math.log2(r)
-    let logR3 = logR * logR * logR
-
-    return 10 / (constantA + (constantB * logR) + (constantC * logR3))
+  function toCelsius (adc) {
+    return toKelvin(adc) - 273.15
   }
 
   return {
--- test/index.test.js
+++ test/index.test.js
@@ -3,31 +3,38 @@
 const assert = require('assert')
 const converter = require('../converter.js')
 
-describe('should conver to acd to celsius', function () {
-  it('to celsius', function () {
-    let val = 500
-    assert.equal(converter.toCelsius(val), -755.9838235622623)
+describe('should conver to adc to celsius', function () {
+  it('adc 512, should give ~25 celsius', function () {
+    assert.equal(converter.toCelsius(512), 25.044031751056878)
+  })
+
+  it('adc 666, should give ~40 celsius', function () {
+    assert.equal(converter.toCelsius(666), 39.83509698019253)
+  })
+
+  it('adc 817, should give ~60 celsius', function () {
+    assert.equal(converter.toCelsius(817), 59.61975249394453)
+  })
+
+  it('adc 935, should give ~90 celsius ', function () {
+    assert.equal(converter.toCelsius(935), 89.1192649219604)
   })
 })
 
-describe('should conver to acd to kelvin', function () {
-  it('100', function () {
-    assert.equal(converter.toKelvin(100), 52.360913671096114)
+describe('should conver to adc to kelvin', function () {
+  it('adc 512, should give ~298 kelvin', function () {
+    assert.equal(converter.toKelvin(512), 298.19403175105685)
   })
 
-  it('200', function () {
-    assert.equal(converter.toKelvin(200), -168.8532311940894)
+  it('adc 666, should give ~312 kelvin', function () {
+    assert.equal(converter.toKelvin(666), 312.9850969801925)
   })
 
-  it('300', function () {
-    assert.equal(converter.toKelvin(300), -299.6207544896333)
+  it('adc 817, should give ~332 kelvin', function () {
+    assert.equal(converter.toKelvin(817), 332.7697524939445)
   })
 
-  it('400', function () {
-    assert.equal(converter.toKelvin(400), -398.362268980728)
-  })
-
-  it('500', function () {
-    assert.equal(converter.toKelvin(500), -482.8338235622623)
+  it('adc 935, should give ~362 kelvin ', function () {
+    assert.equal(converter.toKelvin(935), 362.26926492196037)
   })
 })
